c5e0fdc2acaf01b65a267971032ca0a313400fb0
quantumjot <code@arlowe.co.uk>
2021-07-15 13:45:43 +0100

    another config update

--- vne/model.py
+++ vne/model.py
@@ -7,7 +7,5 @@ class VNEModel:
     def __init__(self):
         pass
 
-    def create_heterogeneous_image(
-        self, dummy: np.ndarray, *args, **kwargs
-    ) -> np.ndarray:
+    def create_sample(self, dummy: np.ndarray, *args, **kwargs) -> np.ndarray:
         return create_heterogeneous_image(*args, **kwargs)
